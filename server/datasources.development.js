49b9b6bcf78a020f00484a7784d1937879af9442
rahulkulmi <rahul@newput.com>
2017-08-23 10:25:17 +0530

    Update development file.

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var MONGO_URI = 'mongodb://lt_db:hSipg7YgSA0YH2Ui@cluster0-shard-00-00-nwvbu.mongodb.net:27017,cluster0-shard-00-01-nwvbu.mongodb.net:27017,cluster0-shard-00-02-nwvbu.mongodb.net:27017/';
-var MONGO_DB = 'DEV_LB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+// var MONGO_URI = 'mongodb://lt_db:hSipg7YgSA0YH2Ui@cluster0-shard-00-00-nwvbu.mongodb.net:27017,cluster0-shard-00-01-nwvbu.mongodb.net:27017,cluster0-shard-00-02-nwvbu.mongodb.net:27017/';
+// var MONGO_DB = 'DEV_LB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 
 module.exports = {
   db: {
@@ -11,8 +11,8 @@ module.exports = {
   mongoDB: {
     name: 'mongoDB',
     connector: 'mongodb',
-    // url: process.env.MONGODB_URL,
-    url: MONGO_URI + MONGO_DB,
+    url: process.env.MONGODB_URL,
+    // url: MONGO_URI + MONGO_DB,
     lazyConnect: false,
   },
   push: {
