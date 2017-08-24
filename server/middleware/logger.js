2e4d742b809385366606ca2ef8477a60ba492aab
rahulkulmi <rahul@newput.com>
2017-08-24 16:19:06 +0530

    Added log folder from code.

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,8 +1,10 @@
 'use strict';
 var winston = require('winston');
+var fs = require('fs');
 var path = require('path');
 
 var logDirectory = path.join(__dirname, '../../logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 winston.emitErrs = true;
 
 var logger = new winston.Logger({
